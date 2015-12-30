68d6d7ff851882e3e31c98c1eaf86f1161177785
akaishi takeshi <t_akaishi@cgate.jp>
2015-12-30 22:43:39 +0900

    manual setlastcallfunc addcustomevnt

--- cocos/scripting/lua-bindings/manual/cocos2d/lua_cocos2dx_manual.cpp
+++ cocos/scripting/lua-bindings/manual/cocos2d/lua_cocos2dx_manual.cpp
@@ -5435,6 +5435,67 @@ tolua_lerror:
 #endif
 }
 
+int tolua_cocos2dx_EventDispatcher_addCustomEventListener(lua_State* tolua_S)
+{
+    int argc = 0;
+    cocos2d::EventDispatcher* cobj = nullptr;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"cc.EventDispatcher",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (cocos2d::EventDispatcher*)tolua_tousertype(tolua_S,1,0);
+
+#if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_EventDispatcher_addCustomEventListener'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 2) 
+    {
+        std::string arg0;
+        std::function<void (cocos2d::EventCustom *)> arg1;
+
+        ok &= luaval_to_std_string(tolua_S, 2,&arg0, "cc.EventDispatcher:addCustomEventListener");
+
+        if(!ok)
+        {
+            tolua_error(tolua_S,"invalid arguments in function 'lua_cocos2dx_EventDispatcher_addCustomEventListener'", nullptr);
+            return 0;
+        }
+        LUA_FUNCTION handler = (  toluafix_ref_function(tolua_S,3,0));
+        cocos2d::EventListenerCustom* ret = cobj->addCustomEventListener(arg0, [=](cocos2d::EventCustom* event){
+            LuaStack* stack = LuaEngine::getInstance()->getLuaStack();
+            stack->pushObject(event, "cc.Ref");
+            
+            stack->executeFunctionByHandler(handler, 1);
+            stack->clean();
+        });
+        object_to_luaval<cocos2d::EventListenerCustom>(tolua_S, "cc.EventListenerCustom",(cocos2d::EventListenerCustom*)ret);
+        return 1;
+    }
+    luaL_error(tolua_S, "%s has wrong number of arguments: %d, was expecting %d \n", "cc.EventDispatcher:addCustomEventListener",argc, 2);
+    return 0;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_EventDispatcher_addCustomEventListener'.",&tolua_err);
+#endif
+
+    return 0;
+}
+
+
 static int tolua_cocos2d_LuaEventListenerCustom_create(lua_State* tolua_S)
 {
     if (nullptr == tolua_S)
@@ -5478,6 +5539,18 @@ tolua_lerror:
     return 0;
 #endif
 }
+
+static void extendEventDispatcher(lua_State* tolua_S)
+{
+    lua_pushstring(tolua_S, "cc.EventDispatcher");
+    lua_rawget(tolua_S, LUA_REGISTRYINDEX);
+    if (lua_istable(tolua_S,-1))
+    {
+        tolua_function(tolua_S,"addCustomEventListener",tolua_cocos2dx_EventDispatcher_addCustomEventListener);
+    }
+    lua_pop(tolua_S, 1);
+}
+
 
 static void extendEventListenerCustom(lua_State* tolua_S)
 {
@@ -7906,6 +7979,7 @@ int register_all_cocos2dx_manual(lua_State* tolua_S)
     extendEventListenerTouchAllAtOnce(tolua_S);
     extendEventListenerMouse(tolua_S);
     extendEventListenerCustom(tolua_S);
+    extendEventDispatcher(tolua_S);
     extendEventListenerAcceleration(tolua_S);
     extendActionCamera(tolua_S);
     extendGridAction(tolua_S);
